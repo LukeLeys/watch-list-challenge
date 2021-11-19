d9f20388eca30129bedb8704d75b2aa35cb6e280
Lukas Leys <lukas.leys.2@gmail.com>
2021-11-19 15:47:25 +0000

    error resolving

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,3 @@ Rails.application.routes.draw do
   resources :bookmarks, only: %i[destroy edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-# config/routes.rb
-Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles, except: :index
-  # ⚠️ don't forget to replace `articles_path` by `root_path` on all the "Back" `link_to`
-end
